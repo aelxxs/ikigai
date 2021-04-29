f6791e5a88b378d8d474e425ce915a4bc54e9860
Alexis <alexis.vielma.us@gmail.com>
2021-04-29 09:37:18 -0500

    🚧 • Introduce Token Positioning

--- src/lib/Lexer.ts
+++ src/lib/Lexer.ts
@@ -8,20 +8,36 @@ export const enum TokenType {
 	TagEnd,
 	Space,
 	Literal,
+	NewLine,
+	Tab,
 }
 
 export interface Token {
 	type: TokenType;
 	value?: string;
+	position?: Position;
+}
+
+export interface Position {
+	line: number;
+	col: number;
+	selected: number;
 }
 
 export type ReadonlyToken = Readonly<Token>;
 export type TokenGenerator = IterableIterator<ReadonlyToken>;
 
+// ? Got a nicer way to handle Token positioning? Open an issue 🎉
+
 export class Lexer {
 	private tokens: Token[] = [];
 	private buffer: string = '';
 	private stream: Stream;
+
+	private position = {
+		line: 1,
+		col: 0,
+	};
 
 	public constructor(str: string) {
 		this.stream = new Stream(str);
@@ -31,11 +47,14 @@ export class Lexer {
 		while (this.stream.next()) {
 			const char = this.stream.peek();
 
+			this.position.col++;
+
 			switch (getCode(char)!) {
 				case getCode('{'): {
 					yield* this.pushToken({
 						type: TokenType.TagStart,
 						value: '{',
+						position: { ...this.position, selected: 1 },
 					});
 					break;
 				}
@@ -43,6 +62,7 @@ export class Lexer {
 					yield* this.pushToken({
 						type: TokenType.Colon,
 						value: ':',
+						position: { ...this.position, selected: 1 },
 					});
 					break;
 				}
@@ -50,6 +70,7 @@ export class Lexer {
 					yield* this.pushToken({
 						type: TokenType.Pipe,
 						value: '|',
+						position: { ...this.position, selected: 1 },
 					});
 					break;
 				}
@@ -57,6 +78,7 @@ export class Lexer {
 					yield* this.pushToken({
 						type: TokenType.TagEnd,
 						value: '}',
+						position: { ...this.position, selected: 1 },
 					});
 					break;
 				}
@@ -64,7 +86,28 @@ export class Lexer {
 					yield* this.pushToken({
 						type: TokenType.Space,
 						value: ' ',
+						position: { ...this.position, selected: 1 },
 					});
+					break;
+				}
+				case getCode('\n'): {
+					// ? Start of a new, reset col to 1.
+					this.position.col = 1;
+					this.position.line++;
+
+					yield* this.pushToken({
+						type: TokenType.NewLine,
+						value: '\n',
+					});
+
+					break;
+				}
+				case getCode('\t'): {
+					yield* this.pushToken({
+						type: TokenType.Tab,
+						value: '\t',
+					});
+
 					break;
 				}
 				default: {
@@ -87,6 +130,11 @@ export class Lexer {
 			const token: Token = {
 				type: TokenType.Literal,
 				value: this.buffer,
+				position: {
+					line: this.position.line,
+					col: this.position.col - this.buffer.length, // ? Start of Buffer column
+					selected: this.buffer.length,
+				},
 			};
 
 			this.tokens.push(token);
