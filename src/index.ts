eee54d529641d49912f483563464be8b32311a20
Alexis <alexis.vielma.us@gmail.com>
2021-05-05 07:44:21 -0500

    🎨 • File Structure Changes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import { Parser, ASTNode } from './lib/Parser';
-
-export function parse(content: string): ASTNode[] {
-	return new Parser(content).parse();
-}
-
 export * from './lib/Lexer';
 export * from './lib/Parser';
+export * from './lib/util';
--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,3 +1,9 @@
+import { ASTNode, Parser } from '../Parser';
+
 export function getCode(str: string): number {
 	return str.charCodeAt(0);
+}
+
+export function parse(content: string): ASTNode[] {
+	return new Parser(content).parse();
 }
